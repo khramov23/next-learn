ac2646e78f005d191c842614c03258be438253c9
Егор Храмов <e.hramov@ase-ec.ru>
2025-05-17 00:37:56 +0300

    first push

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,11 +1,16 @@
-export default function RootLayout({
-  children,
-}: {
-  children: React.ReactNode;
-}) {
+import {ReactNode} from "react";
+import '@/app/ui/global.css'
+import {inter} from "@/app/ui/fonts";
+
+
+interface RootLayoutProps {
+  children: ReactNode
+}
+
+export default function RootLayout({children}: RootLayoutProps) {
   return (
     <html lang="en">
-      <body>{children}</body>
+      <body className={inter.className}>{children}</body>
     </html>
   );
 }
--- app/lib/definitions.ts
+++ app/lib/definitions.ts
@@ -18,11 +18,9 @@ export type Customer = {
 
 export type Invoice = {
   id: string;
-  customer_id: string;
   amount: number;
+  customer_id: string;
   date: string;
-  // In TypeScript, this is called a string union type.
-  // It means that the "status" property can only be one of the two strings: 'pending' or 'paid'.
   status: 'pending' | 'paid';
 };
 
@@ -33,10 +31,10 @@ export type Revenue = {
 
 export type LatestInvoice = {
   id: string;
+  amount: string;
   name: string;
   image_url: string;
   email: string;
-  amount: string;
 };
 
 // The database returns a number for amount, but we later format it to a string with the formatCurrency function
